b90d4b41c25c7f69704bce2508bd4ed90631ce39
Nicola Preda <nicola.preda05@gmail.com>
2022-03-11 20:17:58 +0100

    Updated header

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,61 +1,49 @@
-    <!-- Header -->
-    <header>
-        <nav class="container flex items-center py-4 mt-4 sm:mt-12">
-          <div class="py-1"><img src="./imgs/logo-bookmark.svg" alt="" /></div>
-          <ul class="hidden sm:flex flex-1 justify-end items-center gap-12 text-bookmark-blue uppercase text-xs">
-            <li class="cursor-pointer">Features</li>
-            <li class="cursor-pointer">Pricing</li>
-            <li class="cursor-pointer">Contact</li>
-            <button type="button" class="bg-bookmark-red text-white rounded-md px-7 py-3 uppercase">Login</button>
-          </ul>
-          <div class="flex sm:hidden flex-1 justify-end">
-            <i class="text-2xl fas fa-bars"></i>
-          </div>
-        </nav>
-    </header>
+<header
+	class="bg-gray-800 
+  h-max
+"
+>
+<nav class="container mx-auto">
+  <div class="hidden sm:block sm:ml-6 float-right mb-10">
+    <div class="flex space-x-4">
+      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+      <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium mt-2" aria-current="page">Dashboard</a>
+
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium mt-2">Team</a>
+
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium mt-2">Projects</a>
+
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium mt-2">Calendar</a>
+    </div>
+  </div>
+</nav>
+
+
+<section class="relative pt-6">
+  <div class="container mx-auto flex flex-col lg:flex-row items-center gap-12 mt-14 lg:mt-28">
+    <!-- Content -->
+    <div class="flex flex-1 flex-col items-start pb-24">
+      <h1 class="text-bookmark-blue text-4xl md:text-4 lg:text-5xl text-left mb-6 text-white">
+        I'm Nicola Preda
+      </h1>
+      <p class="text-bookmark-grey text-lg text-left mb-6 text-white">
+        A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites
+        load instantly. Try it for free.
+      </p>
+      <div class="flex justify-center flex-wrap gap-6">
+        <button type="button" class="text-white bg-slate-400 p-3 rounded-lg">
+          Get it on Chrome
+        </button>
+        <button type="button" class="text-white bg-slate-400 p-3 rounded-lg">
+          Get it on Firefox
+        </button>
+      </div>
+    </div>
+    <!-- Image -->
+    <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10 ">
+      <img class="w-3/6 h-3/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full pb-12" src="../static/header.svg" alt="" />
+    </div>
+  </div>
   
-      <!-- Hero -->
-      <section class="relative">
-        <div class="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28">
-          <!-- Content -->
-          <div class="flex flex-1 flex-col items-center lg:items-start">
-            <h2 class="text-bookmark-blue text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">
-              A Simple Bookmark Manager
-            </h2>
-            <p class="text-bookmark-grey text-lg text-center lg:text-left mb-6">
-              A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites
-              load instantly. Try it for free.
-            </p>
-            <div class="flex justify-center flex-wrap gap-6">
-              <button type="button" class="btn btn-purple hover:bg-bookmark-white hover:text-black">
-                Get it on Chrome
-              </button>
-              <button type="button" class="btn btn-white hover:bg-bookmark-purple hover:text-white">
-                Get it on Firefox
-              </button>
-            </div>
-          </div>
-          <!-- Image -->
-          <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
-            <img class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full" src="./imgs/hero-bg.png" alt="" />
-          </div>
-        </div>
-        <!-- Rounded Rectangle -->
-        <div
-          class="
-            hidden
-            md:block
-            overflow-hidden
-            bg-bookmark-purple
-            rounded-l-full
-            absolute
-            h-80
-            w-2/4
-            top-32
-            right-0
-            lg:
-            -bottom-28
-            lg:-right-36
-          "
-        ></div>
-      </section>
+</section>
+</header>
