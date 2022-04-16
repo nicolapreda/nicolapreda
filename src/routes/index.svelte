a71af04d9a74409f6cc0c421702130d92ef9870b
Nicola Preda <nicola.preda05@gmail.com>
2022-04-16 23:19:47 +0200

    Added main Svelte project

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,13 @@
-<header
-	class="bg-gray-800 
-  h-max
-"
->
-<nav class="container mx-auto">
-  <div class="hidden sm:block sm:ml-6 float-right mb-10">
-    <div class="flex space-x-4">
-      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-      <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium mt-2" aria-current="page">Dashboard</a>
-
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium mt-2">Team</a>
-
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium mt-2">Projects</a>
-
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium mt-2">Calendar</a>
+<div class="header bg-indigo-900 h-screen">
+    <!--Porfile pic-->
+    <div class="w-1/4 flex justify-center">
+      <img
+        src="https://avatars1.githubusercontent.com/u/527098?s=460&v=4"
+        class="rounded-full h-32 w-32"
+        alt="Profile Pic"
+      />
     </div>
-  </div>
-</nav>
 
+    <h1 class="text-5xl text-white flex justify-center">Nicola Preda</h1>
 
-<section class="relative pt-6">
-  <div class="container mx-auto flex flex-col lg:flex-row items-center gap-12 mt-14 lg:mt-28">
-    <!-- Content -->
-    <div class="flex flex-1 flex-col items-start pb-24">
-      <h1 class="text-bookmark-blue text-4xl md:text-4 lg:text-5xl text-left mb-6 text-white">
-        I'm Nicola Preda
-      </h1>
-      <p class="text-bookmark-grey text-lg text-left mb-6 text-white">
-        A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites
-        load instantly. Try it for free.
-      </p>
-      <div class="flex justify-center flex-wrap gap-6">
-        <button type="button" class="text-white bg-slate-400 p-3 rounded-lg">
-          Get it on Chrome
-        </button>
-        <button type="button" class="text-white bg-slate-400 p-3 rounded-lg">
-          Get it on Firefox
-        </button>
-      </div>
-    </div>
-    <!-- Image -->
-    <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10 ">
-      <img class="w-3/6 h-3/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full pb-12" src="../static/header.svg" alt="" />
-    </div>
-  </div>
-  
-</section>
-</header>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,7 @@
 module.exports = {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-        extend: {
-<<<<<<< HEAD
-            height: {
-                '128': '32rem',
-            }
-=======
-            colors: {
-                'header-dark': '#243c5a',
-                'header-light': '#E6F0FF',
-                'light-button': '#0F1B4C',
-                'light-heading': '#000336',
-                'light-text': '#5A6473'
-            },
-            fontFamily: {
-                'poppins': 'Poppins, sans-serif',
-            },
-            height: {
-                '128': '32rem',
-            }
-
->>>>>>> main
-        }
+        extend: {}
     },
     plugins: []
 };
