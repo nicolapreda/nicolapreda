333c3986d8df3aae9cc3c18721fb483e76cc09a9
Nicola Preda <nicola.preda05@gmail.com>
2022-05-02 17:41:13 +0200

    Updated header

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,31 @@
-<div class="header bg-indigo-900 h-screen">
-    <!--Porfile pic-->
-    <div class="w-1/4 flex justify-center">
-      <img
-        src="https://avatars1.githubusercontent.com/u/527098?s=460&v=4"
-        class="rounded-full h-32 w-32"
-        alt="Profile Pic"
-      />
+<script>
+  import logo from "./pp.svg";
+</script>
+
+<div class="bg-gray-50 min-h-screen px-16 flex items-center justify-center">
+  <div class="grid grid-cols-2">
+    <div class="intro">
+      <h3 class="font-light font-4xl">Hello, I'm Nicola Preda</h3>
+      <h1 class="font-light">Developer & Designer</h1>
+      <p class="font-light">
+        I'm a web developer and designer based in Milan, Italy.
+      </p>
     </div>
 
-    <h1 class="text-5xl text-white flex justify-center">Nicola Preda</h1>
-
+    <div class="relative w-full max-w-lg">
+      <div
+        class="absolute top-0 -left-4 w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"
+      />
+      <div
+        class="absolute top-0 -right-4 w-96 h-96 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"
+      />
+      <div
+        class="absolute -bottom-8 left-20 w-96 h-96 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"
+      />
+      <div class="m-8 relative space-y-4">
+        <!--profile pic-->
+        <img src={logo} alt="profile" class="w-96 h-96" />
+      </div>
+    </div>
+  </div>
 </div>
