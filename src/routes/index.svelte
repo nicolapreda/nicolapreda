1d5ff6a63f6c248dbc61342007cb4750fe80fac0
Nicola Preda <nicola.preda05@gmail.com>
2022-05-05 18:00:52 +0200

    Updated hero section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script>
   import logo from "./pp.svg";
   import heading from "./heading.svg";
+  import about from "./about-me2.jpg";
 </script>
 
 <section class="h-300">
@@ -12,7 +13,7 @@
       class="isLoaded w-full h-240"
     />
   </div>
-  <div class="navbar py-5 mx-auto w-full absolute z-20">
+  <div class="navbar py-5 w-full absolute z-20 mx-32">
     <div class="grid grid-cols-2 gap-4">
       <div class="navbar-brand ">
         <a href="index.html" class="flex items-center justify-start">
@@ -41,7 +42,7 @@
     </div>
   </div>
 
-  <div class="grid md:grid-cols-2 w-full gap-16 absolute z-10 mx-auto ">
+  <div class="grid md:grid-cols-2 w-full gap-16 absolute z-10 mx-auto px-32">
     <div class="mt-64">
       <p class=" text-2xl">Hello, I’m Nicola Preda</p>
       <h1
@@ -68,26 +69,80 @@
 
 <!--hero section-->
 <section>
-  <div class="container mx-auto px-4">
+  <div class="container mx-32">
     <div class="grid grid-cols-2 gap-16">
       <div>
         <!--about img-->
-        <img
-          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-          alt="about"
-          class="w-full h-auto"
-        />
+        <img src={about} alt="about" class="w-full h-auto" />
       </div>
 
       <div>
         <h2 class="text-3xl font-bold text-gray-900">about</h2>
-
-        <p class=" text-gray-600">
-          I’m a full-stack developer and designer with a passion for creating
-          beautiful and intuitive user experiences.
+        <p class=" text-gray-600 mt-5">
+          Aliquip dolore velit quis cillum dolor anim veniam labore officia
+          ullamco reprehenderit. Eiusmod minim sit aute laborum qui culpa nulla.
+          Quis occaecat et dolore enim tempor eu. Ullamco ipsum ullamco
+          voluptate est labore anim quis mollit proident elit. Do consequat quis
+          eu tempor consequat sint labore mollit reprehenderit adipisicing
+          consequat sit laboris. Sint Lorem excepteur proident anim sint nisi
+          velit.
         </p>
       </div>
     </div>
+  </div>
+</section>
+
+<!--roadmap education, experience and awards-->
+
+<section>
+  <!--education-->
+  <div class="mx-32 mt-32">
+    <h1 class="text-indigo-600 font-semibold text-4xl">Education</h1>
+    <div class="border-l-4 border-indigo-500">
+      <div
+        class="h-4 w-4 absolute mt-10 bg-indigo-500 rounded-full"
+        style="margin-left: -10px;"
+      />
+      <div class="pl-16 my-10">
+        <p class="text-xl font-light">2019 - present</p>
+        <h1 class="text-3xl font-semibold">ITIS P. Paleocapa - Bergamo</h1>
+        <h2 class="text-2xl font-normal">Computer expert</h2>
+        <p class="text font-normal">
+          Id ex qui excepteur enim. Incididunt eu reprehenderit ipsum ex
+          consectetur id. Ut id et fugiat reprehenderit exercitation irure qui
+          ea do culpa eu. Voluptate est enim deserunt aute pariatur.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <!--experience-->
+
+  <div class="mx-32 mt-32">
+    <h1 class="text-indigo-600 font-semibold text-4xl">Experience</h1>
+    <div class="border-l-4 border-indigo-500">
+      <div
+        class="h-4 w-4 absolute mt-10 bg-indigo-500 rounded-full"
+        style="margin-left: -10px;"
+      />
+      <div class="pl-16 my-10">
+        <p class="text-xl font-light">2019 - present</p>
+        <h1 class="text-3xl font-semibold">ITIS P. Paleocapa - Bergamo</h1>
+        <h2 class="text-2xl font-normal">Computer expert</h2>
+        <p class="text font-normal">
+          Id ex qui excepteur enim. Incididunt eu reprehenderit ipsum ex
+          consectetur id. Ut id et fugiat reprehenderit exercitation irure qui
+          ea do culpa eu. Voluptate est enim deserunt aute pariatur.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <!--awards-->
+
+  <div class="mx-32 mt-32">
+    <h1 class="text-indigo-600 font-semibold text-4xl">Awards</h1>
+
   </div>
 </section>
 
