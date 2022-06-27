7e44304854b39a3e686ab46df61a14a1c10893d2
Nicola <nicola.preda05@gmail.com>
2022-06-28 00:35:16 +0200

    Updated classes on homepage section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,7 @@
 <script>
   import logo from "./assets/img/pp.svg";
-  import heading from "./assets/img/heading.svg";
   import about from "./assets/img/about-me2.jpg";
   import project1 from "./assets/img/Yunime.svg";
-  import hero from "./assets/img/hero.jpg";
 </script>
 
 <navbar>
@@ -25,60 +23,68 @@
       <div>
         <a
           href="about.html"
-          class="ml-10 hover:border-b-4 hover:border-indigo-500 transition">About</a
+          class="ml-10 hover:border-b-4 hover:border-indigo-500 transition"
+          >About</a
         >
         <a
           href="projects.html"
-          class="ml-10 hover:border-b-4 hover:border-indigo-500 transition">Contact Me</a
+          class="ml-10 hover:border-b-4 hover:border-indigo-500 transition"
+          >Contact Me</a
         >
         <a
           href="projects.html"
-          class="ml-10 hover:border-b-4 hover:border-indigo-500 transition">Projects</a
+          class="ml-10 hover:border-b-4 hover:border-indigo-500 transition"
+          >Projects</a
         >
       </div>
       <div>
-        <a href="/blog" class="px-4 py-2 bg-indigo-200 hover:bg-indigo-400 transition rounded-lg">Blog</a>
+        <a
+          href="/blog"
+          class="px-4 py-2 bg-indigo-200 hover:bg-indigo-400 transition rounded-lg"
+          >Blog</a
+        >
       </div>
     </div>
   </div>
 </navbar>
 
-  <div class="grid grid-cols-2 px-64 py-32">
-  
-    <div>
-      <h1 class=" leading-normal text-4xl font-normal">Hello, I'm</h1>
-      <h1 class="leading-normal text-6xl font-bold text-indigo-500">
-        Nicola Preda.
-      </h1>
-      <h1 class=" text-3xl font-light">
-        I'm a novice programmer and designer, and I love creating projects.
-      </h1>
-
-    </div>
-
-      <div>
-        <img src={hero} alt="Hero image">
-
-      </div>
-    
-  </div>
+<div class="text-center px-64 py-32">
+  <h1 class=" leading-normal text-4xl font-normal">Hello, I'm</h1>
+  <h1 class="leading-normal text-6xl font-bold text-indigo-500">
+    Nicola Preda.
+  </h1>
+  <h1 class=" text-3xl font-light">
+    I'm a novice programmer and designer, and I love creating projects.
+  </h1>
+</div>
 
 <section>
   <div class="bg-indigo-50 p-32">
-    <div class="grid grid-cols-2 gap-4 mx-32">
+    <div class="grid grid-cols-2 gap-16 mx-32">
       <div>
         <!--about img-->
-        <img src={about} alt="about" class="w-128 h-auto drop-shadow-md rounded-md" />
+        <img
+          src={about}
+          alt="about"
+          class="w-128 h-auto drop-shadow-md rounded-md"
+        />
       </div>
 
       <div>
         <h2 class="text-4xl font-bold text-gray-900">About Me</h2>
         <p class=" text-gray-600 mt-5 text-xl font-normal leading-relaxed">
-          I'm a student that loves programming, In my spare time I love learning new things, coding and implement my ideas. I love traveling and visiting new places ✈️, going to the mountains 🧗‍♂️, listening to music and reading. Finally, I love enjoying every single moment of my life doing what I love, sharing time with the people who make me feel happy and being myself. 😉 
+          I'm a student that loves programming, In my spare time I love learning
+          new things, coding and implement my ideas. I love traveling and
+          visiting new places ✈️, going to the mountains 🧗‍♂️, listening to music
+          and reading. Finally, I love enjoying every single moment of my life
+          doing what I love, sharing time with the people who make me feel happy
+          and being myself. 😉
         </p>
         <div class="mt-10">
-          <a class="bg-indigo-200 px-7 py-5 rounded-2xl drop-shadow-md text-xl" href="#">Download CV</a>
-
+          <a
+            class="bg-indigo-200 px-7 py-5 rounded-2xl drop-shadow-md text-xl"
+            href="#">Download CV</a
+          >
         </div>
       </div>
     </div>
@@ -87,98 +93,114 @@
 
 <!--roadmap education, experience and awards-->
 
-<section>
-  <!--education-->
-  <div class="mx-32 mt-32">
-    <h1 class="text-indigo-600 font-semibold text-4xl">Education</h1>
-    <div class="border-l-4 border-indigo-500">
-      <div
-        class="h-4 w-4 mt-10 bg-indigo-500 rounded-full"
-        style="margin-left: -10px;"
-      />
-      <div class="pl-16 my-10">
-        <p class="text-xl font-light">2019 - present</p>
-        <h1 class="text-3xl font-semibold">ITIS P. Paleocapa - Bergamo</h1>
-        <h2 class="text-2xl font-normal">Computer expert</h2>
-        <p class="text font-normal">
-          Id ex qui excepteur enim. Incididunt eu reprehenderit ipsum ex
-          consectetur id. Ut id et fugiat reprehenderit exercitation irure qui
-          ea do culpa eu. Voluptate est enim deserunt aute pariatur.
+<section class="px-64 grid grid-cols-3 mt-32 gap-16">
+  <div >
+    <div class="grid grid-rows-3">
+      <a href="#education" class="hover:border-b-4 hover:border-indigo-500 transition">Education</a>
+      <a href="#education" class="hover:border-b-4 hover:border-indigo-500 transition">Experience</a>
+      <a href="#education" class="hover:border-b-4 hover:border-indigo-500 transition">Awards</a>
+  
+    </div>
+
+
+  </div>
+  
+  
+  <div class="col-span-2">
+    <!--education-->
+    <div>
+      <h1 class="text-indigo-600 font-semibold text-4xl">Education</h1>
+      <div class="border-l-4 border-indigo-500">
+        <div
+          class="h-4 w-4 mt-10 bg-indigo-500 rounded-full"
+          style="margin-left: -10px;"
+        />
+        <div class="pl-16 my-10">
+          <p class="text-xl font-light">2019 - present</p>
+          <h1 class="text-3xl font-semibold">ITIS P. Paleocapa - Bergamo</h1>
+          <h2 class="text-2xl font-normal">Computer expert</h2>
+          <p class="text font-normal">
+            Id ex qui excepteur enim. Incididunt eu reprehenderit ipsum ex
+            consectetur id. Ut id et fugiat reprehenderit exercitation irure qui
+            ea do culpa eu. Voluptate est enim deserunt aute pariatur.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!--experience-->
+
+    <div class=" mt-32">
+      <h1 class="text-indigo-600 font-semibold text-4xl">Experience</h1>
+      <div class="border-l-4 border-indigo-500">
+        <div
+          class="h-4 w-4 mt-10 bg-indigo-500 rounded-full"
+          style="margin-left: -10px;"
+        />
+        <div class="pl-16 my-10">
+          <p class="text-xl font-light">2019 - present</p>
+          <h1 class="text-3xl font-semibold">ITIS P. Paleocapa - Bergamo</h1>
+          <h2 class="text-2xl font-normal">Computer expert</h2>
+          <p class="text font-normal">
+            Id ex qui excepteur enim. Incididunt eu reprehenderit ipsum ex
+            consectetur id. Ut id et fugiat reprehenderit exercitation irure qui
+            ea do culpa eu. Voluptate est enim deserunt aute pariatur.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!--awards-->
+
+    <div class=" mt-32">
+      <h1 class="text-indigo-600 font-semibold text-4xl">Awards</h1>
+      <div class="p-5 bg-gray-50 my-5 shadow-lg rounded-lg">
+        <p>
+          Eu Lorem ullamco duis ex ea in incididunt nisi ut enim adipisicing
+          aliqua. Labore irure ex veniam dolore eiusmod occaecat labore labore
+          reprehenderit consequat sunt id laboris aute. Deserunt minim
+          exercitation enim nulla pariatur commodo occaecat sint mollit mollit
+          consectetur magna velit ullamco.
+        </p>
+      </div>
+      <div class="p-5 bg-gray-50 my-5 shadow rounded-lg">
+        <p>
+          Eu Lorem ullamco duis ex ea in incididunt nisi ut enim adipisicing
+          aliqua. Labore irure ex veniam dolore eiusmod occaecat labore labore
+          reprehenderit consequat sunt id laboris aute. Deserunt minim
+          exercitation enim nulla pariatur commodo occaecat sint mollit mollit
+          consectetur magna velit ullamco.
+        </p>
+      </div>
+
+      <div class="p-5 bg-gray-50 my-5 shadow rounded-lg">
+        <p>
+          Eu Lorem ullamco duis ex ea in incididunt nisi ut enim adipisicing
+          aliqua. Labore irure ex veniam dolore eiusmod occaecat labore labore
+          reprehenderit consequat sunt id laboris aute. Deserunt minim
+          exercitation enim nulla pariatur commodo occaecat sint mollit mollit
+          consectetur magna velit ullamco.
+        </p>
+      </div>
+
+      <div class="p-5 bg-gray-50 my-5 shadow rounded-lg">
+        <p>
+          Eu Lorem ullamco duis ex ea in incididunt nisi ut enim adipisicing
+          aliqua. Labore irure ex veniam dolore eiusmod occaecat labore labore
+          reprehenderit consequat sunt id laboris aute. Deserunt minim
+          exercitation enim nulla pariatur commodo occaecat sint mollit mollit
+          consectetur magna velit ullamco.
         </p>
       </div>
     </div>
   </div>
 
-  <!--experience-->
 
-  <div class="mx-32 mt-32">
-    <h1 class="text-indigo-600 font-semibold text-4xl">Experience</h1>
-    <div class="border-l-4 border-indigo-500">
-      <div
-        class="h-4 w-4 mt-10 bg-indigo-500 rounded-full"
-        style="margin-left: -10px;"
-      />
-      <div class="pl-16 my-10">
-        <p class="text-xl font-light">2019 - present</p>
-        <h1 class="text-3xl font-semibold">ITIS P. Paleocapa - Bergamo</h1>
-        <h2 class="text-2xl font-normal">Computer expert</h2>
-        <p class="text font-normal">
-          Id ex qui excepteur enim. Incididunt eu reprehenderit ipsum ex
-          consectetur id. Ut id et fugiat reprehenderit exercitation irure qui
-          ea do culpa eu. Voluptate est enim deserunt aute pariatur.
-        </p>
-      </div>
-    </div>
-  </div>
-
-  <!--awards-->
-
-  <div class="mx-32 mt-32">
-    <h1 class="text-indigo-600 font-semibold text-4xl">Awards</h1>
-    <div class="p-5 bg-gray-50 my-5 shadow-lg rounded-lg">
-      <p>
-        Eu Lorem ullamco duis ex ea in incididunt nisi ut enim adipisicing
-        aliqua. Labore irure ex veniam dolore eiusmod occaecat labore labore
-        reprehenderit consequat sunt id laboris aute. Deserunt minim
-        exercitation enim nulla pariatur commodo occaecat sint mollit mollit
-        consectetur magna velit ullamco.
-      </p>
-    </div>
-    <div class="p-5 bg-gray-50 my-5 shadow rounded-lg">
-      <p>
-        Eu Lorem ullamco duis ex ea in incididunt nisi ut enim adipisicing
-        aliqua. Labore irure ex veniam dolore eiusmod occaecat labore labore
-        reprehenderit consequat sunt id laboris aute. Deserunt minim
-        exercitation enim nulla pariatur commodo occaecat sint mollit mollit
-        consectetur magna velit ullamco.
-      </p>
-    </div>
-
-    <div class="p-5 bg-gray-50 my-5 shadow rounded-lg">
-      <p>
-        Eu Lorem ullamco duis ex ea in incididunt nisi ut enim adipisicing
-        aliqua. Labore irure ex veniam dolore eiusmod occaecat labore labore
-        reprehenderit consequat sunt id laboris aute. Deserunt minim
-        exercitation enim nulla pariatur commodo occaecat sint mollit mollit
-        consectetur magna velit ullamco.
-      </p>
-    </div>
-
-    <div class="p-5 bg-gray-50 my-5 shadow rounded-lg">
-      <p>
-        Eu Lorem ullamco duis ex ea in incididunt nisi ut enim adipisicing
-        aliqua. Labore irure ex veniam dolore eiusmod occaecat labore labore
-        reprehenderit consequat sunt id laboris aute. Deserunt minim
-        exercitation enim nulla pariatur commodo occaecat sint mollit mollit
-        consectetur magna velit ullamco.
-      </p>
-    </div>
-  </div>
 </section>
 
 <!--current projects-->
 <section>
-  <div class="mt-32 mx-32 flex justify-center">
+  <div class="mt-32 flex justify-center px-64">
     <div class="grid md:grid-cols-3 gap-32 place-content-center">
       <div class="max-w-sm rounded overflow-hidden shadow-lg">
         <img class="w-full" src={project1} alt="Yunime" />
