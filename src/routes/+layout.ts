dba2a33a160e7dd20bc74b5c1bce9d484293e61e
Nicola Preda <nicola.preda05@gmail.com>
2023-12-15 23:19:34 +0100

    Added Vercel Speed Insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,10 @@
 import { dev } from '$app/environment';
 
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 inject({ mode: dev ? 'development' : 'production' });
+injectSpeedInsights();
 
 export async function load({ url }) {
 	return {
