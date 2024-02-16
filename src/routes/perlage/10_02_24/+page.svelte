02a42e69a447a2895624ff37bcb9c7076813ed3e
Nicola Preda <nicola.preda05@gmail.com>
2024-02-16 17:33:28 +0100

    Update +page.svelte

--- src/routes/perlage/10_02_24/+page.svelte
+++ src/routes/perlage/10_02_24/+page.svelte
@@ -2,7 +2,7 @@
 	<title>Perlage 10/02/24</title>
 	<meta property="og:title" content="Perlage 10/02/24" />
 	<meta property="og:description" content="Foto Perlage 10/02/24">
-	<meta property="og:image" content="%sveltekit.assets%/perlage/10-02-24/00042.jpg">
+	<meta property="og:image" content="../%sveltekit.assets%/perlage/10-02-24/00042.jpg">
 	<meta name=description content="Foto Perlage 10/02/24">
 
 </svelte:head>
@@ -24,41 +24,11 @@
 		samucam.push(`/perlage/10-02-24/samu/${i.toString().padStart(5, '0')}.JPG`);
 	}
 
-	let imageUrl;
-  const username = 'nicolapreda';
-  const password = 'vQcFycebLFej6XH';
-
-  async function fetchImage() {
-    try {
-      const response = await fetch('https://drive.predanicola.it/remote.php/dav/files/nicolapreda/Foto/Sony%20a6100/Perlage/10-02-24/DSC03014-Migliorato-NR.jpg', {
-        headers: {
-          'Authorization': 'Basic ' + btoa(`${username}:${password}`),
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
-        }
-      });
-      
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      
-      const blob = await response.blob();
-      imageUrl = URL.createObjectURL(blob);
-    } catch (error) {
-      console.error('Error fetching image:', error);
-    }
-  }
-
-  fetchImage();
+  
 
 </script>
 
 <section class="mx-[5%] md:mx-[10%] my-12">
-	{#if imageUrl}
-	<img src={imageUrl} alt="Fetched Image" />
-  {:else}
-	<p>Loading...</p>
-  {/if}
-
   
 	<div class="grid md:grid-cols-4 gap-4 mt-12">
 		{#each images as image}
