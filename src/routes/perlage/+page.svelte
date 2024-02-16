1cf6cfc28922990072a593dac6657453bfcde279
Nicola Preda <nicola.preda05@gmail.com>
2024-02-16 11:02:51 +0100

    Updated "perlage" event section

--- src/routes/perlage/+page.svelte
+++ src/routes/perlage/+page.svelte
@@ -3,22 +3,21 @@
 </script>
 
 <section class="mx-[5%] md:mx-[20%] my-12">
-	<h1 class="text-8xl">TUTTI GLI EVENTI</h1>
 
 	<div class="grid xl:grid-cols-3 gap-16 xl:gap-4 mt-12">
-		<div class="bg-secondarypink rounded-3xl shadow">
-			<div class="bg-primary rounded-t-3xl flex">
-				<img src="perlage/10-02-24/00042.jpg" alt="Evento 10-02-24" class="h-full w-full object-cover" />
+		<div class="bg-white rounded-3xl shadow-xl">
+			<div class="bg-primary rounded-3xl flex">
+				<img src="perlage/10-02-24/00042.jpg" alt="Evento 10-02-24" class="h-full w-full object-cover rounded-t-3xl" />
 			</div>
 			<div class="p-5 content-center text-center">
 				<div>
-					<h1 class="text-primary lg:text-4xl md:text-3xl text-2xl">10 Febbraio 2024</h1>
+					<h1 class="text-primary lg:text-3xl text-2xl">10 Febbraio 2024</h1>
 				</div>
-				<div class="mt-5">
+				<div class="my-5">
 					<a
 						href="/perlage/10_02_24"
-						class="px-4 py-2 bg-secondary hover:bg-secondary-hover transition rounded-3xl"
-						>Visualizza tutto</a
+						class="px-4 py-2 bg-primary hover:bg-primary-hover transition rounded-2xl text-white"
+						>VISUALIZZA TUTTO</a
 					>
 				</div>
 			</div>
@@ -26,4 +25,3 @@
 	</div>
 </section>
 
-<Footer />
--- src/routes/perlage/10_02_24/+page.svelte
+++ src/routes/perlage/10_02_24/+page.svelte
@@ -1,10 +1,9 @@
 <script>
-// @ts-nocheck
-
+		// @ts-nocheck
 	import Footer from '../../footer.svelte';
 
 	let samucam = [];
-	
+
 	let images = [];
 
 	for (let i = 1; i <= 146; i++) {
@@ -12,22 +11,21 @@
 	}
 
 	for (let i = 1; i <= 65; i++) {
-		samucam.push(`/perlage/10-02-24/samu/${i.toString().padStart(5, '0')}.JPG`);
+		samucam.push(`/perlage/10-02-24/samu/${i.toString().padStart(5, '0')}.jpg`);
 	}
 </script>
-
-<style>
-    .zoomable-link:hover .zoomable-img {
-        transform: scale(1.2); /* Effetto di zoom del 20% */
-    }
-</style>
 
 <section class="mx-[5%] md:mx-[10%] my-12">
 
 	<div class="grid md:grid-cols-4 gap-4 mt-12">
 		{#each images as image}
 			<a href={image} class="zoomable-link relative inline-block overflow-hidden">
-				<img src={image} alt="perlage" class="w-full h-full object-cover transition-transform duration-300 ease-in-out zoomable-img" loading="lazy" />
+				<img
+					src={image}
+					alt="perlage"
+					class="w-full h-full object-cover transition-transform duration-300 ease-in-out zoomable-img"
+					loading="lazy"
+				/>
 			</a>
 		{/each}
 	</div>
@@ -35,10 +33,21 @@
 	<div class="grid grid-cols-4 gap-4 mt-36">
 		{#each samucam as image}
 			<a href={image} class="zoomable-link relative inline-block overflow-hidden">
-				<img src={image} alt="perlage" class="w-full h-full object-cover transition-transform duration-300 ease-in-out zoomable-img" loading="lazy" />
+				<img
+					src={image}
+					alt="perlage"
+					class="w-full h-full object-cover transition-transform duration-300 ease-in-out zoomable-img"
+					loading="lazy"
+				/>
 			</a>
 		{/each}
 	</div>
 </section>
 
 <Footer />
+
+<style>
+    .zoomable-link:hover .zoomable-img {
+        transform: scale(1.2); /* Effetto di zoom del 20% */
+    }
+</style>
